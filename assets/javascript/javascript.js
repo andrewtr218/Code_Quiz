d0da43d40d472aa91640890831eb91b7c2353099
andrewtr218 <andrewtr218@gmail.com>
2019-12-16 11:28:35 -0600

    Started setting up html and converting it to javascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -12,25 +12,74 @@ var qArr = [
     ///etc.
 ];
 
+// MAKE ARRAY THAT PUTS ANSWERS IN ORDER
+
+
   //write a function that takes a number, asks the question associated with that number, verifies the answer for correctedness against users input, and returns true or false depending on the result
 
-var qNum = prompt("Enter the number of the question you would like to pick") - 1;
+// var ansNum = ("What answer would you like to pick?")
 
-function ask(qNum){
-    var qTitle = qArr[qNum].title;
-    var options = qArr[qNum].choices;
-    var ans = qArr[qNum].answer;
-    alert(qTitle);
-    alert(options[0]);
-    alert(options[1]);
-    alert(options[2]);
-    alert(options[3]);
-    alert(ans);
-    alert(qNum);
-    alert("javascript is working");
+function ask(){
+
+    // var qTitle = qArr[qNum].title;
+    // var options = qArr[qNum].choices;
+    // var ans = qArr[qNum].answer;
+    // alert(qTitle);
+    // alert(options[0]);
+    // alert(options[1]);
+    // alert(options[2]);
+    // alert(options[3]);
+    // alert(ans);
+    // alert(qNum);
+    // alert("javascript is working");
 
     // return qTitle, options, ans
+    
+    // alert(qTitle);
+    // alert(qTitle);
+    // alert(options[0]);
+    // alert(options[1]);
+    // alert(options[2]);
+    // alert(options[3]);
+    // alert(ans);
+    // alert(qNum);
+    // alert("javascript is working");
+
+init();
+
+function ansCheck1(){
+  if (document.getElementById("ans" + (2)).textContent === qArr[0].choices[1]){
+    alert("the comparison works");
+  document.getElementById("ans1").addEventListener("click",next());
 };
 
+    // for verification for javascript click action try setting attribute (may add permanence). Otherwise make the answer array.
+    // if (document.getElementById("ans" + (2)).textContent === qArr[0].choices[1]){
+    //     alert("the comparison works");
+    //     document.getElementById("ans1").addEventListener("click",next());
+    // };
+    
+    // for(i = 0;) i < 2;)
+// add on-click to take me to next question
+return a
+};
 
-ask(qNum);
+function next(){
+    alert("next works");
+    // if (ansUser === qArr[0].answer[qNum-1]){
+    //     alert("Javascript got this far")
+    //     return(true)
+    // }
+};
+
+ask();
+ansCheck1()
+
+function init(){}
+for(i = 0; i < 4; i++){
+
+  document.getElementById("ans" + (i + 1)).innerText = qArr[0].choices[i];
+  document.getElementById("ans" + (i + 1)).innerText = qArr[0].choices[i];
+  document.getElementById("ans" + (i + 1)).innerText = qArr[0].choices[i];
+  document.getElementById("ans" + (i + 1)).innerText = qArr[0].choices[i];
+  };
