2bf9f5596f29619f54327186d136f0d2fdfb123b
andrewtr218 <andrewtr218@gmail.com>
2019-12-19 05:35:01 -0600

    Adjusted code structure after tutoring session. Code is still buggy and now unstyled. Need to clarify some procedures. index.txt is the previous version of index.html. The new javascript sheet used is javascript 3.

--- assets/javascript/javascript2.js
+++ assets/javascript/javascript2.js
@@ -6,22 +6,23 @@ var qArr = [
     },
     {
       title: "What is the combined number of unique letters and numbers on a standard keyboard?",
-      choices: ["6", "46", "Qwerty 2", "43"],
+      choices: ["6", "43", "Qwerty 2", "46"],
       answer: "46"
     },
     ///etc.
 ];
 
-var aArr = [qArr[0] , qArr[1]];
-var a=0;
+// var ansArr = [q[0].answer , q[1].answer];
 
 var score = (0);
-document.getElementById("score").textContent=("Score: " + 0);
-
+document.getElementById("score").textContent=("Score: " + score);
+var a = 0;
 
 function init(a){
     for(i = 0; i < 4; i++){
-    
+
+
+      // document.getElementById("");
       document.getElementById("ans" + (i + 1)).innerText = qArr[a].choices[i];
       document.getElementById("ans" + (i + 1)).innerText = qArr[a].choices[i];
       document.getElementById("ans" + (i + 1)).innerText = qArr[a].choices[i];
@@ -33,32 +34,107 @@ function init(a){
     
 };
 
-function wrong(){
-    alert("Wrong!");
-        if (score >= 0);{
+// function change_page(){
+//     window.location.href = "highscores.html";
+//   } 
 
-            score += 0;
-        };
+// function wrong(){
+//     alert("Wrong!");
+//     if(a<2){ 
+//         if (score > 0){
 
-        else{
-            score -= 1;
-
-        };
-        document.getElementById("score").textContent = ("Score: " + score);
-        init(1);
-};
-
-function right(){
-    alert("Right!");
-        init(1);
-        score += 1;
-        document.getElementById("score").textContent = ("Score: " + score);
-};
-
-init(0);
+//             score -= 1;
+//             a += 1;
+//         };
 
 
-document.getElementById("ans1").addEventListener("click" ,wrong);
-document.getElementById("ans2").addEventListener("click" ,right);
-document.getElementById("ans3").addEventListener("click" ,wrong);
-document.getElementById("ans4").addEventListener("click" ,wrong);
+        
+
+        
+        
+
+        
+//         document.getElementById("score").textContent = ("Score: " + score);
+//         init(1);
+//     }
+//     else{
+//         // change_page();
+//         document.getElementById("score").textContent = ("Score: " + score);
+//         init(1);
+//     }
+// };
+
+// function right(){
+//     alert("Right!");
+//         init(1);
+//         score += 1;
+//         a +=1;
+//         document.getElementById("score").textContent = ("Score: " + score);
+// };
+
+// function right{
+//       alert("this path works");
+// }
+
+init(a);
+
+document.getElementById("ans1").addEventListener("click" , function(){
+  if (answered == qArr[currentQuestion].answer) {
+    score += 1;
+  } else {
+    score -= 1;
+    timeLeft -= 5;
+
+    if (score < 0){
+      score = 0;
+    }
+  }
+  a += 1;
+  init(a)
+});    
+
+
+
+
+
+// document.getElementById("ans2").addEventListener("click" , function(){
+//   if(answer ==
+//   }
+//   if(a)
+//   }
+//   a += 1;
+// }); 
+
+
+
+
+
+document.getElementById("ans3").addEventListener("click" , function(event){
+  if (answered == qArr[currentQuestion].answer) {
+    score += 1;
+  } else {
+    score -= 1;
+    timeLeft -= 5;
+
+    if (score < 0)
+      score = 0;
+  }
+  a += 1;
+}); 
+
+
+
+
+
+
+document.getElementById("ans4").addEventListener("click" , function(event){
+  if (answered == qArr[currentQuestion].answer) {
+    score += 1;
+  } else {
+    score -= 1;
+    timeLeft -= 5;
+
+    if (score < 0)
+      score = 0;
+  }
+}); 
---
+++ assets/javascript/javascript3.js
@@ -0,0 +1,131 @@
+var qArr = [{
+    title: "What common ghost phrase do ghost programmers say to startle humans?",
+    choices: ["Top of the mornin' to ya!", "BoOOooOOoOoolean!", "Great job sport!", "Aaaaaaaaaaaaaaaaaaaaaaaaagh"],
+    answer: "BoOOooOOoOoolean!"
+  },
+  {
+    title: "What is the combined number of unique letters and numbers on a standard keyboard?",
+    choices: ["6", "43", "Qwerty 2", "46"],
+    answer: "46"
+  },
+];
+
+var playerName = '';
+var playerScore = 0;
+var currentQuestion = 0;
+var timerID = null;
+var timeLeft = 0;
+
+function initializeQuestion() {
+  let currentQ = qArr[currentQuestion];
+  showTimeLeft()
+  document.querySelector('#question-title').innerHTML = currentQ.title
+  document.querySelector('#game-score').innerHTML = playerScore
+
+  flushButtons();
+
+  for (let i = 0; i < currentQ.choices.length; i++) {
+    addButton(currentQ.choices[i], currentQ.answer);
+  }
+}
+
+function addButton(choice, rightAnswer) {
+  let el = document.createElement('div');
+  // add classes on element here
+
+  el.setAttribute('data-choice', choice);
+  el.innerHTML = choice;
+
+  el.addEventListener('click', onButtonClick);
+
+  document.querySelector('#answer-buttons-block').appendChild(el)
+}
+
+function onButtonClick(e) {
+  let target = e.target;
+  let answered = target.getAttribute('data-choice')
+
+  if (answered == qArr[currentQuestion].answer) {
+    playerScore += 1;
+  } else {
+    playerScore -= 1;
+    timeLeft -= 5;
+
+    if (playerScore < 0)
+      playerScore = 0;
+  }
+
+  currentQuestion += 1;
+
+  if (currentQuestion >= qArr.length) {
+    endGame()
+  } else {
+    initializeQuestion()
+  }
+}
+
+function flushButtons() {
+  let block = document.querySelector('#answer-buttons-block')
+  let choices = block.querySelectorAll('div[data-choices]')
+
+  for (let i = 0; i < choices.length; i++) {
+    choices[i].removeEventListener('click', onButtonClick)
+  }
+
+  document.querySelector('#answer-buttons-block').innerHTML = ''
+}
+
+function initializeGame() {
+  playerName = document.querySelector('#name').value;
+
+  document.querySelector('#main-game').removeAttribute('style')
+  document.querySelector('#intro').setAttribute('style', 'display:none;')
+  initializeTimer();
+  initializeQuestion();
+}
+
+function initializeTimer() {
+  timerID = window.setInterval(tick, 1000)
+  timeLeft = 30;
+}
+
+function tick() {
+  timeLeft --;
+  showTimeLeft();
+
+  if (timeLeft <= 0) {
+    endGame();
+  }
+}
+
+function showTimeLeft() {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = (timeLeft - (minutes * 60));
+
+  document.querySelector('#timer').innerHTML = timeLeft + ' seconds';
+}
+
+function endGame() {
+  window.clearInterval(timerID)
+  document.querySelector('#end').removeAttribute('style')
+  document.querySelector('#main-game').setAttribute('style', 'display:none;')
+
+  let scores = JSON.parse(localStorage.getItem('scores') || '[]')
+  let scoreStr = '<table>'
+  scores.push({
+    name: playerName,
+    score: playerScore
+  })
+
+  localStorage.setItem('scores', JSON.stringify(scores))
+
+  for (let i = 0; i < scores.length; i++) {
+      scoreStr += `<tr><td>${scores[i].name}</td><td>${scores[i].score}</td></tr>`
+      // scoreStr += '<td>'+scores+'</td>'
+  }
+
+  scoreStr += '</table>'
+
+  document.querySelector('#end-score').innerHTML = scoreStr
+
+}
